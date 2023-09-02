a2ed1c48539e4c9311dc5e901f73f3c13784d3f1
SachinFSDSD <rajusachin090@gmail.com>
2023-09-02 16:01:18 +0530

    database added

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,6 +1,6 @@
 module.exports = {
   DB_NAME: "movieBooking_db",
-  DB_URL: "mongodb+srv://Sachin:<password>@cluster0.140qg0y.mongodb.net/",
+  DB_URL: "mongodb+srv://Sachin:Welcom1@cluster0.140qg0y.mongodb.net/",
 
   // "mongodb://localhost/movieBooking_db",
 };
